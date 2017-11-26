662555ff6bdacbfa3b4191ef7b2fadf5c3da630e
yuyedaidao <wyqpadding@gmail.com>
2017-11-26 19:40:09 +0800

    Initial Commit

---
+++ YQLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YQLabel
+//
+//  Created by 王叶庆 on 2017/11/26.
+//  Copyright © 2017年 王叶庆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
